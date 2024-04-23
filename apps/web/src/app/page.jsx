9bf2158f16fdd1f06c0ae5df759c2d11edd10db6
Hanief Burhanuddin <haniefburhan17@gmail.com>
2024-04-23 21:29:02 +0700

    build: prettier

---
+++ apps/web/src/app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import styles from './page.module.css';
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center bg-red-200">
+      test
+    </div>
+  );
+}
