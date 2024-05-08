9e5af33523d1089dc9cf5ad9bd487e303c880d6e
Mustofa <45399514+Mustofawho0@users.noreply.github.com>
2024-05-08 21:30:50 +0700

    perf: find all ticket and fetch for voucher event

--- apps/api/src/api/ticket/TicketRouter.ts
+++ apps/api/src/api/ticket/TicketRouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import {
   createTicketController,
+  findTicketController,
   updateTicketController,
 } from './TicketController';
 
@@ -8,5 +9,6 @@ const router = Router();
 
 router.post('/register', createTicketController);
 router.put('/event/edit/:id', updateTicketController);
+router.get('/', findTicketController);
 
 export default router;
